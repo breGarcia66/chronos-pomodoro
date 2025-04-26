d9d7fecae33d750ca0995f2b8b46c8e0904a4db0
Brenno Levy <brennol3vy@gmail.com>
2025-04-26 16:15:13 -0300

    Aprendendo CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Nomes de funções no uso de React deve ser escrito em PascalCase
+import { Heading } from "./components/Heading";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./styles/themes.css";
+import "./styles/global.css";
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quasi sit
+        illo! Beatae maxime ipsam libero veniam unde. Sapiente explicabo quaerat
+        fugit aliquam atque incidunt repellendus doloremque ullam? Harum, odit.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+//export { App } -> Export com nome específico
+//export default App -> Export com nome indefinido
+//export function App -> Outra maneira de export com nome específico
---
+++ src/components/Heading.tsx
@@ -0,0 +1,6 @@
+import Styles from "./Heading.modules.css";
+
+export function Heading() {
+  console.log(Styles);
+  return <h1>Hello, world!!!</h1>;
+}
