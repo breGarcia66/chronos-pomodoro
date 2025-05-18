f5a2dfab92b085d7e5879fb4fe5f5c1fcfa4b149
Brenno Levy <brennol3vy@gmail.com>
2025-05-18 17:57:01 -0300

    Criando 'Container' como um componente react e aplicando CSS modules nele

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //Nomes de funções no uso de React deve ser escrito em PascalCase
 import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
 
 import "./styles/themes.css";
 import "./styles/global.css";
@@ -8,18 +9,9 @@ import { TimerIcon } from "lucide-react";
 export function App() {
   return (
     <>
-      <div className="container-fluid">
-        <div className="container">
-          <div className="content">
-            <section>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Accusantium expedita facere, omnis labore voluptatem dolore
-              officia autem, neque sunt ut, necessitatibus in ipsum. Sequi quam
-              dolores doloremque nulla sed aliquam.
-            </section>
-          </div>
-        </div>
-      </div>
+        <Container>
+          <Heading>Pomodoro</Heading>
+        </Container>
     </>
   );
 }
