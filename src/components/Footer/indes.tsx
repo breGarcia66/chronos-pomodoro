63e0f8aab043b838ad3361f248aa8c100fc7b7ad
Brenno Levy <brennol3vy@gmail.com>
2025-05-27 00:00:39 -0300

    Começando a desenvolver a funcionalidade de dark mode e light mode

--- src/components/Footer/indes.tsx
+++ src/components/Footer/indes.tsx
@@ -3,8 +3,8 @@ import styles from "./styles.module.css";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href="">Entenda como funciona a técnica pomodoro</a>
-      <a href="">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💙</a>
+      <a href="">Understand how the Pomodoro technique works</a>
+      <a href="">Chronos Pomodoro &copy; {new Date().getFullYear()} - Made with 💙</a>
     </footer>
   );
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import { House, History, Settings, Sun } from "lucide-react";
 
 import styles from "./styles.module.css";
 
+type AvailableThemes = "light" | "dark";
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    console.log(theme);
+  }
+  
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a 
+        className={styles.menuLink} 
+        href="#" aria-label="Home" 
+        title="Home"
+      >
         <House />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Task history"
+        title="Task history"
+      >
         <History />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Settings"
+        title="Settings"
+      >
         <Settings />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Light/dark theme"
+        title="Light/dark theme"
+        onClick={handleThemeChange}
+      >
         <Sun />
       </a>
     </nav>
