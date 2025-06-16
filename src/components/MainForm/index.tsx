163fb1e721591e6f9f3deff55066250ba0dadf9a
Brenno Levy <brennol3vy@gmail.com>
2025-06-16 01:47:50 -0300

    Testando input controlado

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,24 +2,26 @@ import { DefaultInput } from "../DefaultInput";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { PlayCircle } from "lucide-react";
+import { useState } from "react";
 
 export function MainForm() {
-  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>){
+  const [taskName, setTaskName] = useState("");
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('Testando evento de submeter página...')
+    console.log("Testando evento de submeter página...");
   }
 
   return (
-    <form
-    onSubmit={handleCreateNewTask} 
-    className="taskForm" 
-    action="">
+    <form onSubmit={handleCreateNewTask} className="taskForm" action="">
       <div className="formRow">
         <DefaultInput
           id="myInput"
           type="text"
           labelText="Task:"
           placeholder="Type something..."
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
           required
         />
       </div>
